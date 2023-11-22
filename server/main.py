317c5233b5487acf71aa38a52ce0dbda043b0892
SX <sunnyxiayf@126.com>
2023-11-22 21:09:42 +0000

    msg:use direct input to type the received character

--- server/main.py
+++ server/main.py
@@ -1,9 +1,10 @@
 import asyncio
 from websockets.server import serve
+import pydirectinput as pi
 
 async def main(websocket):
     async for message in websocket:
-        print(message)
+        pi.press(message)
         
 
 async def connect():
